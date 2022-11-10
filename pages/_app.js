921488880b4653fafbae9b61dfc153abe9deef52
Saeed Nemati <sn.1361@gmail.com>
2022-11-10 05:27:17 +0330

    committed by script

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Favicon from '../components/Favicon'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <Favicon />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
