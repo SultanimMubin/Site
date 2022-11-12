b6201ff876065e762847c8a5a7da7d56e9b5686d
Saeed Nemati <sn.1361@gmail.com>
2022-11-12 09:03:49 +0330

    committed by script

--- app/layout.js
+++ app/layout.js
@@ -5,9 +5,14 @@ function MyApp({ children }) {
     return <html>
         <head>
             <Favicon />
+            <title>Sultanimmubin</title>
+            <meta name="description" content="Observation, thought, impartiality" />
         </head>
         <body>
             {children}
+            <footer>
+                footer
+            </footer>
         </body>
     </html>
 }
